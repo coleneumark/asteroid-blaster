e9fc2c6c78e6b88c4b23b03ba3567751bed70a75
cormacelynch <cormac.lynch1@ucdconnect.ie>
2023-03-02 20:04:17 +0000

    first attempt at space ship shape

---
+++ createPolygon.java
@@ -0,0 +1,50 @@
+package pack1;
+
+//source: https://www.educative.io/answers/what-is-drawpolygon-in-java
+
+import java.awt.*;
+import javax.swing.*;
+
+public class createPolygon extends JPanel{
+	 public void paintComponent(Graphics g) {
+		 	//Player Space Ship
+		 	int [] xp = {10, 15, 20, 20, 30, 30, 35, 40, 40, 30, 25, 20, 10};
+	        int [] yp = {15, 25, 25, 20, 20, 25, 25, 15, 30, 30, 45, 30, 30};
+	        double scale = 3;
+	        for (int i =0;i<xp.length;i++) {
+	        	xp[i] = (int) (xp[i] * scale);
+	        	yp[i] = (int) (yp[i] * scale);
+	        }
+	        Polygon playerPoly = new Polygon(xp,yp,xp.length);
+	        g.drawPolygon(playerPoly);
+	        
+	      //Enemy Space Ship
+	        int [] xe = {20, 40, 50, 60, 70, 80, 100, 80, 70, 60, 50, 40};
+	        int [] ye = {60, 40, 10, 40, 10, 40, 60, 80, 110, 80, 110, 80};
+	        scale = 0.5;
+	        for (int i =0;i<xe.length;i++) {
+	        	xe[i] = (int) (xe[i] * scale);
+	        	ye[i] = (int) (ye[i] * scale);
+	        }
+	        Polygon enemyPoly = new Polygon(xe,ye,xe.length);
+	        g.drawPolygon(enemyPoly);
+	        
+	        //Large Asteroid
+	       
+	        //Medium Asteroid
+	        
+	        //Small Asteroid
+	      }
+	    	
+	 public static void main( String args[] ) {
+	        //JFrame.setDefaultLookAndFeelDecorated(true);
+	        JFrame frame = new JFrame("Draw Space Ship");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        //frame.setBackground(Color.white);
+	        frame.setSize(300, 300);
+
+	        createPolygon panel = new createPolygon();
+	        frame.add(panel);
+	        frame.setVisible(true);
+	    }
+}
